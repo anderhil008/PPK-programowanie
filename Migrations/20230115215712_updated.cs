a366315ccc9d9f3f0f1c819d3c6177bfc7da5d35
48603 <48603@LAPTOP-3T141TEL>
2023-06-10 17:11:18 +0200

    Dodaj pliki projektów.

---
+++ Migrations/20230115215712_updated.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    public partial class updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "Task",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Task",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Task");
+        }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using MessagePack;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication1.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskContent { get; set; }
+        public string UserId { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
